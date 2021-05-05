e576e77bfd0d0314b29043095e2db0f117392914
Aaron Nelson <axnelly55@gmail.com>
2021-05-05 03:42:42 -0700

    the src and public folders

---
+++ src/robots.js
@@ -0,0 +1,122 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  },
+  {
+    id: 11,
+    name: 'Herbert Henry',
+    username: 'H&Hry',
+    email: 'HenryHerbert@gle.me'
+  },
+  {
+    id: 12,
+    name: 'Fantazi Flrety',
+    username: 'Fanflret',
+    email: 'flret_fant@azi.io'
+  },
+  {
+    id: 13,
+    name: 'Meloneay Glerty',
+    username: 'Glemelo',
+    email: 'Glert@melo.biz'
+  },
+  {
+    id: 14,
+    name: 'Kleetus McVeeneer',
+    username: 'Kleetimus',
+    email: 'Kleet@prime.me'
+  },
+  {
+    id: 15,
+    name: 'Niboonya Sleerk',
+    username: 'Slerboon',
+    email: 'Niboo_slerk@yan.io'
+  },
+  {
+    id: 16,
+    name: 'Skylapsen Pogoban',
+    username: 'Skyban',
+    email: 'Nook.Sky@kban.biz'
+  },
+  {
+    id: 17,
+    name: 'Mcgriffert Peteana',
+    username: 'Petamcgrif',
+    email: 'griffpet@ena.me'
+  },
+  {
+    id: 18,
+    name: 'Greasint Globert',
+    username: 'Globetrotter',
+    email: 'Greeglobe@bert.io'
+  },
+  {
+    id: 19,
+    name: 'Slime Mcgrime ',
+    username: 'Slizzy.Mcgrizzy',
+    email: 'Captn.Slime@grime.biz'
+  },
+  {
+    id: 20,
+    name: 'Moe Boejangles',
+    username: 'MB.Jang',
+    email: 'Big.Moe@Jangjang.biz'
+  }
+];
